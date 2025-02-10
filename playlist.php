072da30b777e58696f0aca1f6177c22301686e02
Yuvraj <130395237+yuvraj824@users.noreply.github.com>
2025-02-10 19:09:44 +0530

    Update playlist.php

--- playlist.php
+++ playlist.php
@@ -10,7 +10,7 @@ $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVE
 $host = $_SERVER['HTTP_HOST'];
 $requestUri = $_SERVER['REQUEST_URI'];
 $currentScript = basename($_SERVER['SCRIPT_NAME']);
-$baseMpdUrl = $protocol . $host . str_replace($currentScript, 'manifest.mpd', $requestUri);
+$baseMpdUrl = $protocol . $host . str_replace($currentScript, 'manifest.php', $requestUri);
 $baseWvUrl = $protocol . $host . str_replace($currentScript, 'widevine.php', $requestUri);
 
 $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
